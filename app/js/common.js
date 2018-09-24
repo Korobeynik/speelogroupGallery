cad3dcfe6e22b694416a547f7627383cca4e5658
Korobeynik <korobeynik77@gmail.com>
2018-09-24 23:21:56 +0300

    Init project.Add libs

---
+++ app/js/common.js
@@ -0,0 +1,15 @@
+$(function() {
+
+	// $('.button--buy, .button--white').addClass('popup');
+	// $('.button--buy, .button--white').attr('href', '#form');
+	$('.popup').magnificPopup({
+		 removalDelay: 500, //delay removal by X to allow out-animation
+		  callbacks: {
+		    beforeOpen: function() {
+		       this.st.mainClass = this.st.el.attr('data-effect');
+		    }
+		  },
+		  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+	});
+
+});
