42ab39f8a44f491db7f16fe9effe8f5b480548c9
Korobeynik <korobeynik77@gmail.com>
2018-09-26 18:18:57 +0300

    Create mobile version

--- app/js/common.js
+++ app/js/common.js
@@ -1,33 +1,19 @@
 $(function() {
 
 	$('.popup').magnificPopup({
-  callbacks: {
+		fixedBgPos: true,
+  	callbacks: {
     open: function() {
     	// Owl Carousel
-			  var owl = $(".news-card");
+			  let owl = $(".news-card");
 			  owl.owlCarousel({
 			    items: 1,
 			    margin: 0,
 			    loop: false,
 			    nav: true,
+					nav: true,
 			    mouseDrag: false,
-			    responsive:{
-            0:{
-                items:1,
-                nav:true,
-                autoplay:false,
-                mouseDrag: true
-            },
-            600:{
-                items:1,
-                nav:false,
-                autoplay:false,
-                mouseDrag: true
-            },
-            1200:{
-                items:1
-            }
-        },
+			    touchDrag: true,
 			    navText: [
 					  '<span class="arrow-left"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="45" viewBox="0 0 24 45"><path fill="#FFF" fill-rule="evenodd" d="M20.89 22.499L.379 2.176a1.26 1.26 0 0 1 0-1.801 1.29 1.29 0 0 1 1.817 0l21.426 21.228a1.26 1.26 0 0 1 0 1.801L2.196 44.623c-.247.245-.58.377-.904.377a1.26 1.26 0 0 1-.904-.377 1.261 1.261 0 0 1 0-1.801L20.89 22.499z"/></svg></span>',
 					  '<span class="arrow-right"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="45" viewBox="0 0 24 45"><path fill="#FFF" fill-rule="evenodd" d="M20.89 22.499L.379 2.176a1.26 1.26 0 0 1 0-1.801 1.29 1.29 0 0 1 1.817 0l21.426 21.228a1.26 1.26 0 0 1 0 1.801L2.196 44.623c-.247.245-.58.377-.904.377a1.26 1.26 0 0 1-.904-.377 1.261 1.261 0 0 1 0-1.801L20.89 22.499z"/></svg></span>'
@@ -36,19 +22,53 @@ $(function() {
 			     if (e.namespace && e.property.name === 'position' 
 					&& e.relatedTarget.relative(e.property.value) === e.relatedTarget.items().length - 1) {
 					// put your stuff here ...
+					console.log(this);
+					$(this).touchDrag = false;
 					$('.news-card-item.finish-slide').addClass('active');
 					$('.owl-nav').addClass('active');
 					$('.owl-dots').addClass('active');
 					$('.news-main-title').addClass('hide');
-					console.log('last slide')
+					//$('body').on("touchmove", false);
+		
+
+
+
+
+					console.log('last slide');
+					owl.owlCarousel();
+					//$('.news-card').data('owlCarousel').settings.touchDrag = false;
+					//$('.news-card').data('owlCarousel').options.touchDrag = false;
+					//var $carousel = 
+					//$('.news-card').data('owl.carousel').options.loop = true;
+					var simple = $('.news-card');
+					//simple.owlCarousel.touchDrag = false;
+					//$('.news-card').trigger('change', { property: { name: 'touchDrag', value: false } });
+					$('.news-card').data('owl.carousel').options.nav = false;
+					$('.news-card').trigger( 'refresh.owl.carousel' );
+
+					if ($(window).width() < 800) {
+			    	if($('.news-card-item.finish-slide').hasClass('active')) {
+			    		$('.news-main-title').removeClass('hide');
+
+			    	}
+			    }
 
 					}
+			  }).on('dragged.owl.carousel', function(e) {
+			  	var el = $('.owl-item.active').find('.news-card-item');
+			  	 if($(el).hasClass('dark-top')) {
+				  	$('.news-main-title').addClass('dark');
+				  	$('.mfp-close-btn-in .mfp-close').addClass('dark');
+				  } else {
+				  	$('.news-main-title').removeClass('dark');
+				  	$('.mfp-close-btn-in .mfp-close').removeClass('dark');
+				  }
+
 			  });
 
       $.magnificPopup.instance.close = function() {
        $.magnificPopup.proto.close.call(this);
        goToStart();
-        // Call the original close method to close the popup
       };
     }
   }
@@ -74,6 +94,8 @@ $(function() {
 		$('.owl-dots').removeClass('active');
 		$('.news-card-item.finish-slide').removeClass('active');
 		$('.news-main-title').removeClass('hide');
+		//$('body').unbind('touchmove');
+		
 	}
 
 });
@@ -81,7 +103,9 @@ $(function() {
 $(window).on('load', function () {
 
     if ($(window).width() < 800) {
-
+    	if($('.news-card-item.finish-slide').hasClass('active')) {
+    		$('.news-main-title').removeClass('hide');
+    	}
     }
 
 });
